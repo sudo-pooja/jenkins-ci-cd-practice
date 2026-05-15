4f60f760918123f998c8bd0e4758a7c5de423162
sudo-pooja <poojasree.ps.30@gmail.com>
2026-05-15 21:27:55 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require("http");
 const server = http.createServer((req,res) => {
-    res.end('<h1>Aloha Kiddo...!</h1>');
+    res.end('<h1>Aloha...!</h1>');
 });
 server.listen(3000, () => console.log('App is running...!'))
