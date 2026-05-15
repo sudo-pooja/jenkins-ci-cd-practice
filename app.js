7d58b3971bb6b4069031a54bee65c90af6ff1f29
sudo-pooja <poojasree.ps.30@gmail.com>
2026-05-15 18:46:19 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require("http");
 const server = http.createServer((req,res) => {
-    res.end('<h1>Deployed by Jenkins!</h1>');
+    res.end('<h1>This is a demo for the CI-CD with Jenkins!</h1>');
 });
 server.listen(3000, () => console.log('App is running on port 3000'))
